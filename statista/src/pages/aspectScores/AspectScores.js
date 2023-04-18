85610c02686ec44af93f68889623f13508dea599
Carolina Calle <calle.carolina@gmail.com>
2023-04-18 17:34:06 +0200

    Add aspect scores layout

--- statista/src/pages/aspectScores/AspectScores.js
+++ statista/src/pages/aspectScores/AspectScores.js
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from 'react';
+import { useState, useEffect } from "react";
+import '../../pages/aspectScores/AspectScores.css';
+import rawData from '../../data/data.json';
+import Filter from '../../components/filter/Filter';
+import AspectBox from '../../components/aspectBox/AspectBox';
+import SuscriptionBox from '../../components/aspectBox/Suscription';
+import Reviews from '../../components/reviews/Reviews';
+import StarsView from '../../components/reviews/StarsView';
+import ReviewsBar from '../../components/reviews/ReviewsBar';
 
-function Aspects() {
+export default function Aspects() {
+  
+  const[activeSelect, setActiveSelect]=useState([]);
+  const[aspectsData, setAspectsData]=useState([]);
+  const[reviewsData, setReviewsData]=useState([]);
+
+  useEffect(()=>{    
+    if (activeSelect.length === 3){
+      let filterData = rawData.data.filter(
+        e => e.deviceType === activeSelect[0] 
+        && e.brand === activeSelect[1]
+        && e.model === activeSelect[2])
+        console.log("FilterData")
+        console.log(filterData)
+        if (filterData.length > 0) {
+          setAspectsData(filterData[0].aspects)
+          setReviewsData(filterData[0].reviews)
+        } else {
+          setAspectsData(null)
+        }
+      }    
+    },[activeSelect]);
+
     return (
-      <div>
-        <h1>Aspects</h1>
+      <div className="aspectScores">
+        <Filter setActiveSelect={setActiveSelect}/>
+          <div className="data-container">
+            <h2> Aspect-based sentiment: BBQ </h2>
+            <div className="data-box">
+              <div className="data-left">
+                <SuscriptionBox/>
+                {
+                  (reviewsData != null && reviewsData.summary != null) ?
+                  <div className="reviews-container">
+                    <Reviews/>
+                    <StarsView value={reviewsData.summary.value} total={reviewsData.summary.total}/>
+                    <ReviewsBar/>   
+                  </div>
+                  : null 
+                }
+              </div>
+            <div className="data-right">
+          {
+          (aspectsData !== null) ?
+            aspectsData.map((aspect,index)=> {
+              return(                
+                  <AspectBox  title={aspect.name} subtitle="Score out of 100" value={aspect.value}/>
+                  )
+                })
+              :null
+          }
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
   
-  export default Aspects;
