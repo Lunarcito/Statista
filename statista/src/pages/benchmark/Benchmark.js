d62d2ded45653b149ef86dec89ddde3d320ae91f
Carolina Calle <calle.carolina@gmail.com>
2023-04-19 01:29:57 +0200

    Add Benchmark page

--- statista/src/pages/benchmark/Benchmark.js
+++ statista/src/pages/benchmark/Benchmark.js
@@ -1,9 +1,28 @@
-import React from 'react'
+import React from 'react';
+import '../../pages/benchmark/Benchmark.css';
+import Filter from '../../components/filter/Filter';
+import rawData from '../../data/data.json';
+import Reviews from '../../components/reviews/Reviews';
+import StarsView from '../../components/reviews/StarsView';
+import ReviewsBar from '../../components/reviews/ReviewsBar';
 
 function Benchmark() {
     return (
-      <div>
-         <h1>Benchmark</h1>
+      <div className='datab-container'>         
+         <Filter setActiveSelect={(()=>{})}/>
+         <h2>Benchmark analysis:BBQ</h2>
+         <div className='datab-box'>
+          <div className='datab-left'>
+                <Reviews/>
+                <StarsView value={rawData.data[0].reviews.summary.value} total={rawData.data[0].reviews.summary.total}/>
+                <ReviewsBar value={rawData.data[0].reviews.details}/>
+          </div>
+          <div className='datab-right'>
+                <Reviews/>
+                <StarsView value={rawData.data[1].reviews.summary.value} total={rawData.data[1].reviews.summary.total}/>
+                <ReviewsBar value={rawData.data[1].reviews.details}/>
+          </div>
+         </div>
       </div>
     );
   }
